a7336c7e2ecfcd4aff5b8910d95d82ecff921adc
leadermin <ms@program.kr>
2014-06-27 11:32:08 +0900

    first

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,15 @@
+class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+    def new
+    end
+
+    def create
+	reder plain: params[:article].inspect
+    end
+    
+    def show
+	@article = Article.find(params[:id])
+    end
+end
